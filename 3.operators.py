d3459887acfab1bffb4bd47c761affa49a4bf35b
furqan-jan <152903705+furqan-jan@users.noreply.github.com>
2026-01-21 12:04:51 +0530

    Add files via upload

---
+++ 3.operators.py
@@ -0,0 +1,34 @@
+     # Arithmetic operators
+"""a=int (input("enter first no : ")) # for user defined input
+b=int (input("enter second no : "))"""
+# a=4
+# b=2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)    # remainder
+# print(a**b)   # a^b
+     # Relational operators
+# c=70
+# d=40
+# print(c==d)
+# print(c!=d)
+# print(c>=d)
+# print(c<=d)
+# print(c>d)    
+# print(c<d)
+     # Assignment operators   
+# num =10
+# num += 10 #num=num+10
+# #num -= 10 #num=num-10
+# #num *= 10 #num=num*10
+# #num /= 10 #num=num/10
+# print (num)     
+     # Logical operators
+x=True
+y=False
+print(not x)
+print(not y)
+print ("And operator: ",x and y)
+print ("OR operator : ",x or y)
