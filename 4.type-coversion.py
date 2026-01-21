a6239fe824ae270d9a0fc73504c42b3992c95bce
furqan-jan <152903705+furqan-jan@users.noreply.github.com>
2026-01-21 13:02:07 +0530

    Add files via upload

---
+++ 4.type-coversion.py
@@ -0,0 +1,12 @@
+          #Type conversion :Automatically converts the data type
+# a=2  # int type
+# b=4.5  # float type 
+# sum=a+b  # float type
+# print (sum)
+# print (type(sum))
+          #Type casting : Manually converts the data type
+a=str( 2 )
+b= "pi"
+c=float(4)
+print (a+b)
+print(type (c))
