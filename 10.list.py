94ef884f3f59c7f5bcf48c93c6868c94471cfaf5
furqan-jan <152903705+furqan-jan@users.noreply.github.com>
2026-01-24 12:36:27 +0530

    Add files via upload

---
+++ 10.list.py
@@ -0,0 +1,14 @@
+# List is a built in data type,it can store elements of different data types.
+marks=[10,20,30,40,50,60]
+print(len(marks))
+print(marks)
+print(marks[1])
+info=['Furqan',20,'Maisuma']
+info[0]="Mohd Furqan"
+print(info)
+    # List Slicing:
+print(marks [0:4])
+print(marks [:])
+print(marks [:3])
+print(marks [1:])
+print(marks [-4:-1])
