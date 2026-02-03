27b61379a2b0ee9dbb3080a994a10fedb8763da3
furqan-jan <152903705+furqan-jan@users.noreply.github.com>
2026-02-03 12:00:20 +0530

    Add files via upload

---
+++ 13.Programs2.py
@@ -0,0 +1,27 @@
+# WAP to ask the User to enter the names of 3 favorite movies and store them in list:
+# movies=[]
+# mov1=input("Enter 1st movie: ")
+# mov2=input("Enter 2nd movie: ")
+# mov3=input("Enter 3rd movie: ")
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+# print(movies)
+
+# WAP to check if a list contains a palindrome of elements:
+# list1=[1,2,2,1]
+# list2=[1,2,3,4]
+# a=list1.copy()
+# a.reverse()
+# if(list1==a):
+#     print ("palindrome")
+# else:
+#     print("Not a palindrome")
+
+# WAP to count the number of students with grade A in the following tuple:
+grade=('A','B','C','A','C','E','A','D','C','B')
+print(grade.count('A'))
+#Store the above values in a list and sort them in order:
+grade=['A','B','C','A','C','E','A','D','C','B']
+grade.sort()
+print(grade)
