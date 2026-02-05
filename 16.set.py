e99e15264e374e587148c5ca101b120b1260e638
furqan-jan <152903705+furqan-jan@users.noreply.github.com>
2026-02-05 21:08:54 +0530

    Add files via upload

---
+++ 16.set.py
@@ -0,0 +1,8 @@
+# Set is the collection of the unordered items.
+# Each element in the set is must be unique and immutable.
+a={1,2,2,3,4,"hello","world","hello"} # Repeated elements in the set is stored only once.
+print(a)
+print(type(a))
+print(len(a))
+b=set() #Empty set syntax
+print(b)
